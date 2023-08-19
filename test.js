31440865302207a806ab54c66697a0c68911c760
david <davidmatovu88@gmail.com>
2023-08-19 15:28:15 +0300

    use the node global object

--- test.js
+++ test.js
@@ -1,9 +1,18 @@
 const name = "david";
-console.log(name);
+console.log(global);
 
 const greet = (name) => {
-  console.log(`Hello ${name}`);
+console.log(`Hello ${name}`);
 };
 
-greet('matovu')
-greet('david')
+greet("matovu");
+greet("david");
+
+
+global.setTimeout(() => {
+    console.log("Time out")
+}, 3000);
+
+const int = setInterval(()=>{
+console.log("My name")
+}, 1000)
