bd8e68662aa1fac71a57785d2388006dbdf66190
david <davidmatovu88@gmail.com>
2023-08-20 20:05:01 +0300

    creating folders and writing files

--- files.js
+++ files.js
@@ -7,8 +7,20 @@ fs.readFile('./docs/blog1.txt', (error,data)=>{
     }
     console.log("my data",data.toString())
 })
-// writing files
 
+// writing files
+fs.writeFile('./docs/blog1.txt', "Hello World", ()=>{
+    console.log("File has been written")
+})
+fs.writeFile('./docs/blog2.txt', "Hello my people", ()=>{
+    console.log("File has been written")
+})
 // directories
+fs.mkdir('./assets', (err) =>{
+    if(err){
+        console.log(err)
+    }
+    console.log("Directory has been created")
+})
 
 // deleting files
