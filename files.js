50bbd023e10f140027fd8e73d69cd52583019acd
david <davidmatovu88@gmail.com>
2023-08-20 20:14:00 +0300

    delete folder if it exists

--- files.js
+++ files.js
@@ -1,26 +1,36 @@
-const fs = require('fs')
+const fs = require("fs");
 
 // reading files
-fs.readFile('./docs/blog1.txt', (error,data)=>{
-    if(error){
-        console.log(error)
-    }
-    console.log("my data",data.toString())
-})
+fs.readFile("./docs/blog1.txt", (error, data) => {
+  if (error) {
+    console.log(error);
+  }
+  console.log("my data", data.toString());
+});
 
 // writing files
-fs.writeFile('./docs/blog1.txt', "Hello World", ()=>{
-    console.log("File has been written")
-})
-fs.writeFile('./docs/blog2.txt', "Hello my people", ()=>{
-    console.log("File has been written")
-})
+fs.writeFile("./docs/blog1.txt", "Hello World", () => {
+  console.log("File has been written");
+});
+fs.writeFile("./docs/blog2.txt", "Hello my people", () => {
+  console.log("File has been written");
+});
 // directories
-fs.mkdir('./assets', (err) =>{
-    if(err){
-        console.log(err)
+
+if (!fs.existsSync("./assets")) {
+  fs.mkdir("./assets", (err) => {
+    if (err) {
+      console.log(err);
     }
-    console.log("Directory has been created")
-})
+    console.log("Directory has been created");
+  });
+} else {
+  fs.rmdir("./assets", (err) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log("folder deleted");
+  });
+}
 
 // deleting files
