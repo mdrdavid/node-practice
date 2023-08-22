319d73e0388980f2c5f815cfd0e0ad44d321e914
david <davidmatovu88@gmail.com>
2023-08-22 08:20:30 +0300

    send html pages to the browser

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const http = require("http");
+const fs = require("fs");
 
 const hostname = "localhost"; // points at '127.0.0.1' which points directly to the computer
 const port = 3000;
@@ -9,16 +10,28 @@ const server = http.createServer((req, res) => {
   console.log("Request has been made");
   res.statusCode = 200;
   //set header content type
-//   res.setHeader("Content-Type", "text/plain");
+  //   res.setHeader("Content-Type", "text/plain");
   res.setHeader("Content-Type", "text/html");
   // write to the response
-  res.write('<p>Hello My People</p>')
-  res.write('<p>Hello My People Again</p>')
-  // end the response 
-  res.end();
+  // res.write('<p>Hello My People</p>')
+  // res.write('<p>Hello My People Again</p>')
 
+  // send an html file to the browser
+  fs.readFile("./views/index.html", (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end();
+
+    } else {
+      // write to the response
+      res.write(data);
+
+      // end the response
+      res.end();
+    }
+  });
 });
 
-server.listen(port,hostname, () => {
+server.listen(port, hostname, () => {
   console.log(`Listening to ${hostname}:${port}`);
 });
