133bf07105fa5c0ac27bcaf880e12f45e3ea91d3
david <davidmatovu88@gmail.com>
2023-08-22 09:12:16 +0300

    add status codes to responses

--- server.js
+++ server.js
@@ -22,19 +22,24 @@ const server = http.createServer((req, res) => {
   switch (req.url) {
     case "/":
       path += "index.html";
+      res.statusCode = 200;
       break;
 
     case "/about":
       path += "about.html";
+      res.statusCode = 200;
       break;
     case "/services":
       path += "services.html";
+      res.statusCode = 200;
       break;
     case "/contactus":
       path += "contactus.html";
+      res.statusCode = 200;
       break;
     default:
       path += "404.html";
+      res.statusCode = 404;
       break;
   }
 
